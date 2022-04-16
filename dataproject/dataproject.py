5a3f6871febac4918cc4d85307d63f3a019704b2
yapicikerem <qbn393@alumni.ku.dk>
2022-04-16 16:42:59 +0200

    changes made

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,15 +1,9 @@
-!pip install -q yfinance
-!pip install -q yahoofinancials
-!pip install -q pandas-datareader
 import pandas as pd
+from IPython.display import display
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 import numpy as np
-import pandas_datareader as pdr
-import datetime as dt
-
-%load_ext autoreload
-%autoreload 2
-
-def normalize_data(df):
-    return df/df.iloc[0, :]
+import datetime
+import random
