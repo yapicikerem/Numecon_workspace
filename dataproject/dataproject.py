c2c8d2245b1adbe6cfcca180918d2a84d554f30e
yapicikerem <qbn393@alumni.ku.dk>
2022-04-16 22:54:53 +0200

    made changes

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,9 +1,15 @@
+!pip install -q yfinance
+!pip install -q yahoofinancials
+!pip install -q pandas-datareader
 import pandas as pd
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 import numpy as np
 import pandas_datareader as pdr
-import datetime as datetime
+import datetime as dt
+
+%load_ext autoreload
+%autoreload 2
 
 def normalize_data(df):
     return df/df.iloc[0, :]
